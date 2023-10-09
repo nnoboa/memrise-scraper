298f9333878c505eb55c9ec8b7912bae14510d85
nnoboa <nicholas.noboa@gmail.com>
2023-10-08 20:52:25 -0400

    update variable names

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -4,12 +4,12 @@ import requests
 import lxml
 
 BASE_URL = "https://app.memrise.com"
-FILENAME = "JLPT-N2.csv"
+FILENAME = "FILENAME.csv"
 
-# N5 - https://app.memrise.com/course/554/jlpt-n5-vocab/
-# N4 - https://app.memrise.com/course/122925/jlpt-n4-readings/
-# N3 - https://app.memrise.com/course/1584135/jlpt-n3-readings-with-audio/
-# N2 - https://app.memrise.com/course/24151/jlpt-n2-from-memrise-beta/
+# JLPT N5 - https://app.memrise.com/course/554/jlpt-n5-vocab/
+# JLPT N4 - https://app.memrise.com/course/122925/jlpt-n4-readings/
+# JLPT N3 - https://app.memrise.com/course/1584135/jlpt-n3-readings-with-audio/
+# JLPT N2 - https://app.memrise.com/course/24151/jlpt-n2-from-memrise-beta/
 
 search = "INSERT-COURSE-HERE"
 r = requests.get(search)
@@ -22,11 +22,11 @@ with open(FILENAME, "w") as file:
         level = BASE_URL + level["href"]
         level_r = requests.get(level)
         level_xml = BeautifulSoup(level_r.content, "lxml")
-        words = level_xml.find_all("div", {"class": "things clearfix"})
-        only_words = words[0].find_all("div", {"class": "text"})
-
+        words_html = level_xml.find_all("div", {"class": "things clearfix"})
+        words = words[0].find_all("div", {"class": "text"})
+        
         counter = 1
-        for word in only_words:
+        for word in words:
             if counter % 2 == 0:
                 counter = 1
                 continue
